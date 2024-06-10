f269280002f1ac6357920790c59494faf82be4c2
Muhannad Al-Mozaiger <programmer22muhannad@gmail.com>
2024-06-10 17:35:42 +0300

    Router Dom Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import './layout.scss'
-import Navbar from './components/navbar/Navbar.jsx'
 import HomePage from './routes/homePage/homePage.jsx'
-function App () {
+import {
+  createBrowserRouter,
+  RouterProvider,
+
+} from 'react-router-dom'
+import ListPage from './routes/listPage/listPage.jsx';
+import Layout from './routes/layout/layout.jsx';
+import SinglePage from './routes/singlePage/singlePage.jsx';
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:
+        <Layout />
+      ,
+      children: [
+
+        { path: "/", element: <HomePage /> },
+        { path: "/list", element: <ListPage /> },
+        { path: "/:id", element: <SinglePage /> },
+
+      ]
+    }
+  ]);
+
+
   return (
-    <div className='layout'>
-      <div className='navbar'>
-        <Navbar />
-      </div>
-      <div className='content'>
-        <HomePage />
-      </div>
-    </div>
+
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/routes/listPage/listPage.jsx
@@ -0,0 +1,9 @@
+import './listPage.scss'
+
+function ListPage() {
+  return (
+    <div>listPage</div>
+  )
+}
+
+export default ListPage;

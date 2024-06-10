38a72e3c165a3cd0999003b603052a215777dd10
Muhannad Al-Mozaiger <programmer22muhannad@gmail.com>
2024-06-10 07:18:40 +0300

    Build Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import "./layout.scss"
-import Navbar from "./components/navbar/Navbar.jsx"
-function App() {
+import './layout.scss'
+import Navbar from './components/navbar/Navbar.jsx'
+import HomePage from './routes/homePage/homePage.jsx'
+function App () {
   return (
-    <div className="layout">
+    <div className='layout'>
+      <div className='navbar'>
         <Navbar />
+      </div>
+      <div className='content'>
+        <HomePage />
+      </div>
     </div>
   )
 }
